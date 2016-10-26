7c1cdd849e87fe2dc4fe4a4b80c3c08724afbcc2
Jeremy Kratz <jwkratz@gmail.com>
2016-10-26 14:52:06 -0500

    Make application controller helpers available to apps

--- lib/landlord/engine.rb
+++ lib/landlord/engine.rb
@@ -4,7 +4,15 @@ module Landlord
   class Engine < ::Rails::Engine
     isolate_namespace Landlord
 
-    if Rails.env.development? 
+    config.to_prepare do
+      root = Landlord::Engine.root
+      extenders_path = root + "app/extenders/**/*.rb"
+      Dir.glob(extenders_path) do |file|
+        Rails.configuration.cache_classes ? require(file) : load(file)
+      end
+    end
+
+    if Rails.env.development?
       #OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
     end
   end
