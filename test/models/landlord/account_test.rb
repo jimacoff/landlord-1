91988f546d06190b38b750343cf9b110c4530c5a
Jeremy Kratz <jwkratz@gmail.com>
2017-01-22 15:01:18 -0600

    Stubbed out Account model tests

--- test/models/landlord/account_test.rb
+++ test/models/landlord/account_test.rb
@@ -2,8 +2,41 @@ require 'test_helper'
 
 module Landlord
   class AccountTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+
+    test "should not save account without name" do
+      account = Account.new
+      assert_not account.save, "Saved the account without a name"
+    end
+
+    test "should not save account without plan" do
+      account = Account.new
+      account.name = "Account"
+      assert_not account.save, "Saved the account without a plan"
+    end
+
+    test "should not save account without stripe_id" do
+      account = Account.new
+      account.name = "Account"
+      account.plan = Plan.first
+      assert_not account.save, "Saved the account without a stripe_id"
+    end
+
+    test "should not save account without status" do
+      account = Account.new
+      account.name = "Account"
+      account.plan = Plan.first
+      account.stripe_id = "stripe_id"
+      assert_not account.save, "Saved the account without a status"
+    end
+
+    test "should save valid account" do
+      account = Account.new
+      account.name = "Account"
+      account.plan = Plan.first
+      account.stripe_id = "stripe_id"
+      account.status = "trialing"
+      account.memberships.new(user: User.first)
+      skip("TODO: Setup fake_stripe gem for Stripe method testing")
+    end
   end
 end
