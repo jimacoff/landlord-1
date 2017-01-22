995c245a286431a9169191e547086497311b6628
Jeremy Kratz <jwkratz@gmail.com>
2017-01-22 15:10:47 -0600

    Stubbed out StripeWebhook model tests

--- test/models/landlord/stripe_webhook_test.rb
+++ test/models/landlord/stripe_webhook_test.rb
@@ -2,8 +2,17 @@ require 'test_helper'
 
 module Landlord
   class StripeWebhookTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+
+    test "should not save stripe_webhook without stripe_id" do
+      stripe_webhook = StripeWebhook.new
+      assert_not stripe_webhook.save, "Saved the stripe_webhook without a stripe_id"
+    end
+
+    test "should save valid stripe_webhook" do
+      stripe_webhook = StripeWebhook.new
+      stripe_webhook.stripe_id = "stripe_id"
+      assert stripe_webhook.save, "Saved the stripe_webhook"
+    end
+
   end
 end
