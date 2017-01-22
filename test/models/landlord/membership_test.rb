7c256afd3e1765782d7601c345d909b7564942fb
Jeremy Kratz <jwkratz@gmail.com>
2017-01-22 15:04:27 -0600

    Stubbed out Membership model tests

--- test/models/landlord/membership_test.rb
+++ test/models/landlord/membership_test.rb
@@ -2,8 +2,24 @@ require 'test_helper'
 
 module Landlord
   class MembershipTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+
+    test "should not save membership without account" do
+      membership = Membership.new
+      assert_not membership.save, "Saved the membership without an account"
+    end
+
+    test "should not save membership without user" do
+      membership = Membership.new
+      membership.account = Account.first
+      assert_not membership.save, "Saved the membership without a user"
+    end
+
+    test "should save valid membership" do
+      membership = Membership.new
+      membership.account = Account.first
+      membership.user = User.first
+      assert membership.save, "Saved the membership"
+    end
+
   end
 end
