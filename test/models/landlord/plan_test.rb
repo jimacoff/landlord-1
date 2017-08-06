4cd696ae5bbfc16bcbc21001427dd4f333f63d6f
Jeremy Kratz <jwkratz@gmail.com>
2017-08-06 16:33:17 -0500

    Updated plan and role tests

--- test/models/landlord/plan_test.rb
+++ test/models/landlord/plan_test.rb
@@ -39,7 +39,30 @@ module Landlord
       assert_not plan.save, "Saved the plan without name"
     end
 
-    test "should not save valid plan" do
+    test "should prevent duplicate stripe_ids" do
+      plan1 = Landlord::Plan.new(
+        stripe_id: 'stripe_id_1',
+        amount: 1000,
+        currency: 'usd',
+        interval: 'month',
+        name: 'Plan 1'
+      )
+      assert plan1.save
+
+      plan2 = Landlord::Plan.new(
+        stripe_id: plan1.stripe_id,
+        amount: plan1.amount,
+        currency: plan1.currency,
+        interval: plan1.interval,
+        name: plan1.name
+      )
+      assert_not plan2.save, "Saved the plan with duplicate stripe_id"
+
+      plan2.stripe_id = 'plan_id_2'
+      assert plan2.save, "Saved the plan with unique stripe_id"
+    end
+
+    test "should save valid plan" do
       plan = Landlord::Plan.new
       plan.stripe_id = 'stripe_id_1'
       plan.amount = 1000
--- test/models/landlord/role_test.rb
+++ test/models/landlord/role_test.rb
@@ -2,8 +2,41 @@ require 'test_helper'
 
 module Landlord
   class RoleTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+
+    test "should not save plan without key" do
+      role = Landlord::Role.new
+      assert_not role.save, "Saved the plan without key"
+    end
+
+    test "should not save plan without name" do
+      role = Landlord::Role.new
+      role.key = 'role1'
+      assert_not role.save, "Saved the plan without name"
+    end
+
+    test "should prevent duplicate keys" do
+      role1 = Landlord::Role.new(
+        key: 'role_key_1',
+        name: 'Role 1'
+      )
+      assert role1.save
+
+      role2 = Landlord::Role.new(
+        key: role1.key,
+        name: role1.name
+      )
+      assert_not role2.save, "Saved the role with duplicate key"
+
+      role2.key = 'role_key_2'
+      assert role2.save, "Saved the role with unique key"
+    end
+
+    test "should save plan when valid" do
+      role = Landlord::Role.new
+      role.key = 'role1'
+      role.name = 'Role 1'
+      assert role.save, "Saved the plan with valid attributes"
+    end
+
   end
 end
