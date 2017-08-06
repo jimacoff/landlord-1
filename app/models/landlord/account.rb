e3571cf4a10a2e2369a9286b68202afee924c1bc
Jeremy Kratz <jwkratz@gmail.com>
2017-08-06 15:13:44 -0500

    Refactored account model and stripe webhook handler

--- app/models/landlord/account.rb
+++ app/models/landlord/account.rb
@@ -24,68 +24,85 @@ module Landlord
     after_save :update_stripe_attributes, only: :update
 
     has_settings do |s|
-      s.key :billing, :defaults => { :address => nil, :cc_emails => nil }
+      s.key :billing, defaults: { address: nil, cc_emails: nil }
     end
-
-
 
     # Return the user who is the account owner
     def owner
-      self.memberships.find_by(role: "owner").user
+      m = memberships.find_by(role: Landlord::Role.owner)
+      raise "account has no user assigned as owner" unless m
+      m.user
     end
 
-    # Return boolean indicating if account is currently active, based on billing status
+    # Use billing status to determine if account is active
     def active?
-      self.status != 'past_due' && self.status != 'unpaid' && self.status != 'canceled'
+      status != 'past_due' && status != 'unpaid' && status != 'canceled'
     end
 
+    # Does the account require billing action?
     def billing_error?
-      self.status == 'past_due' || self.status == 'unpaid'
+      status == 'past_due' || status == 'unpaid'
     end
 
     def cancel
-      if self.stripe_id
-        customer = Stripe::Customer.retrieve(self.stripe_id)
-        subscription = customer.subscriptions.first.delete
-        self.status = subscription.status
-        self.save
-      end
+      raise "account has no billing ID" unless stripe_id
+
+      customer = Stripe::Customer.retrieve(stripe_id)
+      subscription = customer.subscriptions.first.delete
+      status = subscription.status
+      save
     end
 
     # Update the account's Stipe customer object
     def update_stripe_attributes
-      if stripe_id && owner
-        customer = Stripe::Customer.retrieve(stripe_id)
-        subscription = customer.subscriptions.first
+      raise "account has no billing ID" unless stripe_id
 
-        customer.email = owner.email
-        customer.description = name
-        customer.save
+      customer = Stripe::Customer.retrieve(stripe_id)
+      subscription = customer.subscriptions.first
 
-        subscription.plan = plan.stripe_id
-        subscription.save
-      end
+      customer.email = owner.email
+      customer.description = name
+      customer.save
+
+      subscription.plan = plan.stripe_id
+      subscription.save
     end
 
+    # Update an account's credit card for billing
     def update_stripe_card(stripe_params)
-      if stripe_id
-        customer = Stripe::Customer.retrieve(stripe_id)
-        subscription = customer.subscriptions.first
+      raise "account has no billing ID" unless stripe_id
+      raise "payment token not provided" unless stripe_params[:token]
 
-        customer.source = stripe_params[:token]
-        customer.save
+      customer = Stripe::Customer.retrieve(stripe_id)
+      subscription = customer.subscriptions.first
 
-        update(
-          status: subscription.status,
-          card_name: stripe_params[:card_name],
-          card_brand: stripe_params[:card_brand],
-          card_last4: stripe_params[:card_last4],
-          card_exp_month: stripe_params[:card_exp_month],
-          card_exp_year: stripe_params[:card_exp_year]
-        )
+      customer.source = stripe_params[:token]
+      customer.save
+
+      update(
+        status: subscription.status,
+        card_name: stripe_params[:card_name],
+        card_brand: stripe_params[:card_brand],
+        card_last4: stripe_params[:card_last4],
+        card_exp_month: stripe_params[:card_exp_month],
+        card_exp_year: stripe_params[:card_exp_year]
+      )
+    end
+
+    # Update an account's credit card from a Stripe credit card object
+    def update_card_from_stripe(card)
+      self.card_name = card.name
+      self.card_brand = card.brand
+      self.card_last4 = card.last4
+      self.card_exp_month = card.exp_month
+      self.card_exp_year = card.exp_year
+
+      if self.changed?
+        self.save
       end
     end
 
+    # Update an account's settings
     def update_settings(account_params)
       account_update_params = account_params.except(:owner_id, :billing_address, :billing_cc_emails)
 
@@ -94,16 +111,17 @@ module Landlord
 
       new_owner_id = account_params[:owner_id]
       current_owner_id = owner.id.to_s
+      change_owner = new_owner_id && new_owner_id != current_owner_id
 
-      if new_owner_id != current_owner_id
+      if change_owner
         new_owner_membership = memberships.find_by(user_id: new_owner_id)
         current_owner_membership = memberships.find_by(user_id: current_owner_id)
 
         if new_owner_membership
-          new_owner_membership.role = "owner"
+          new_owner_membership.role = Landlord::Role.owner
           new_owner_membership.save
 
-          current_owner_membership.role = "admin"
+          current_owner_membership.role = Landlord::Role.admin
           current_owner_membership.save
 
           update_stripe_attributes
@@ -125,11 +143,8 @@ module Landlord
       card = customer.sources.retrieve(customer.default_source)
       account = Account.find_by(stripe_id: customer.id)
 
-      if !account
-        SupportMailer.notification('Stripe event error', 'customer contains Account that does not exist: ' + event.id).deliver_later
-      else
-        account.update_card_from_stripe(card)
-      end
+      raise "Event #{event.id}: customer contains Account that does not exist" unless account
+      account.update_card_from_stripe(card)
     end
 
     # Customer credit card has been updated in Stripe
@@ -139,11 +154,8 @@ module Landlord
       account = Account.find_by(stripe_id: customer.id)
 
       if (customer.default_source == card.id)
-        if !account
-          SupportMailer.notification('Stripe event error', 'customer.card contains Account that does not exist: ' + event.id).deliver_later
-        else
-          account.update_card_from_stripe(card)
-        end
+        raise "Event #{event.id}: customer.card contains Account that does not exist" unless account
+        account.update_card_from_stripe(card)
       end
     end
 
@@ -153,88 +165,53 @@ module Landlord
       customer = Stripe::Customer.retrieve(subscription.customer)
       account = Account.find_by(stripe_id: subscription.customer)
 
-      if !account
-        SupportMailer.notification('Stripe event error', 'customer.subscription contains Account that does not exist: ' + event.id).deliver_later
+      raise "Event #{event.id}: customer.subscription contains Account that does not exist" unless account
+      raise "Event #{event.id}: customer has multiple subscriptions" if customer.subscriptions.count > 1
+
+      # Update the account status if it has changed in Stripe
+      old_status = account.status
+      new_status = subscription.status
+      if old_status != new_status
+        account.status = new_status
+        AccountMailer.past_due(account).deliver_later if new_status == 'past_due'
       end
-      if customer.subscriptions.count > 1
-        SupportMailer.notification('Stripe event error', 'customer has multiple subscriptions: ' + event.id).deliver_later
-      end
 
-      if account
+      # Update the account plan if it has changed in Stripe
+      old_plan = account.plan
+      new_plan = Plan.find_by(stripe_id: subscription.plan.id)
+      raise "Event #{event.id}: customer.subscription contains Plan that does not exist" unless new_plan
+      account.plan = new_plan unless old_plan == new_plan
 
-        # Update the account status if it has changed in Stripe
-        old_status = account.status
-        new_status = subscription.status
-        if old_status != new_status
-          account.status = new_status
-        end
-
-        # Update the account plan if it has changed in Stripe
-        new_plan = Plan.find_by(stripe_id: subscription.plan.id)
-        if !new_plan
-          SupportMailer.notification('Stripe event error', 'customer.subscription contains Plan that does not exist: ' + event.id).deliver_later
-        else
-          if account.plan != new_plan
-            account.plan = new_plan
-          end
-        end
-        
-        # Save account if any changes have been detected from Stripe
-        if account.changed?
-          account.save
-
-          if new_status == 'past_due' && old_status != new_status
-            AccountMailer.past_due(account).deliver_later
-          end
-
-        end
-      end
+      # Save account if any changes have been detected from Stripe
+      account.save if account.changed?
     end
-
-    # Update an account's credit card from a Stripe credit card object
-    def update_card_from_stripe(card)
-      self.card_name = card.name
-      self.card_brand = card.brand
-      self.card_last4 = card.last4
-      self.card_exp_month = card.exp_month
-      self.card_exp_year = card.exp_year
-
-      if self.changed?
-        self.save
-      end
-    end
-
-
 
     private
 
       # Initialize the account owner user and membership relationship
       def init_owner
-        if self.memberships.any?
-          member = self.memberships.first
-          if member
-            existing_user = User.find_by(email: member.user.email)
-            if existing_user
-              # User already exists; use the existing record
-              member.user = existing_user
-            else
-              # New user will receive a welcome email; skip the confirmation email
-              member.user.skip_confirmation_notification!
-            end
-            member.role = "owner"
-          end
+        member = memberships.first if memberships.any?
+        member.role = Landlord::Role.owner if member
+        existing_user = User.find_by(email: member.user.email) if member
+
+        if existing_user
+          # User already exists; use the existing record
+          member.user = existing_user
+        else
+          # New user will receive a welcome email; skip the confirmation email
+          member.user.skip_confirmation_notification!
         end
       end
 
       # Create a Stripe customer for the account
       def init_stripe_attributes
-        if self.memberships.any? && self.memberships.first.valid?
-          owner_user = self.memberships.first.user
-          if !self.stripe_id
-            customer = Stripe::Customer.create(email: owner_user.email, plan: self.plan.stripe_id, description: self.name)
+        if memberships.any? && memberships.first.valid?
+          owner_user = memberships.first.user
+          if !stripe_id
+            customer = Stripe::Customer.create(email: owner_user.email, plan: plan.stripe_id, description: name)
             subscription = customer.subscriptions.first
-            self.stripe_id = customer.id
-            self.status = subscription.status
+            stripe_id = customer.id
+            status = subscription.status
           end
         end
       end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -23,63 +23,75 @@ StripeEvent.configure do |events|
 
   # Occurs whenever any property of a customer changes.
   events.subscribe 'customer.updated' do |event|
-    Landlord::Account.update_from_stripe_customer_event(event)
+    begin
+      Landlord::Account.update_from_stripe_customer_event(event)
+    rescue => ex
+      SupportMailer.notification('Stripe event error', "#{ex.message}").deliver_later
+    end
   end
 
   # Occurs whenever a card's details are changed.
   events.subscribe 'customer.source.updated' do |event|
-    Landlord::Account.update_from_stripe_card_event(event)
+    begin
+      Landlord::Account.update_from_stripe_card_event(event)
+    rescue => ex
+      SupportMailer.notification('Stripe event error', "#{ex.message}").deliver_later
+    end
   end
 
   # Occurs three days before the trial period of a subscription is scheduled to end.
   events.subscribe 'customer.subscription.trial_will_end' do |event|
     subscription = event.data.object
-    account = Landlord::Account.find_by(stripe_id: subscription.customer)
-    if account
-      Landlord::AccountMailer.trial_will_end(account).deliver_later
-    end
+    account = Landlord::Account.find_by(stripe_id: subscription.customer) if subscription
+    Landlord::AccountMailer.trial_will_end(account).deliver_later if account
   end
 
   # Occurs whenever a customer with no subscription is signed up for a plan.
   events.subscribe 'customer.subscription.created' do |event|
-    Landlord::Account.update_from_stripe_subscription_event(event)
+    begin
+      Landlord::Account.update_from_stripe_subscription_event(event)
+    rescue => ex
+      SupportMailer.notification('Stripe event error', "#{ex.message}").deliver_later
+    end
   end
 
   # Occurs whenever a subscription changes. Examples would include switching from one plan to another, or switching status from trial to active.
   events.subscribe 'customer.subscription.updated' do |event|
-    Landlord::Account.update_from_stripe_subscription_event(event)
+    begin
+      Landlord::Account.update_from_stripe_subscription_event(event)
+    rescue => ex
+      SupportMailer.notification('Stripe event error', "#{ex.message}").deliver_later
+    end
   end
 
   # Occurs whenever a customer ends their subscription.
   events.subscribe 'customer.subscription.deleted' do |event|
-    Landlord::Account.update_from_stripe_subscription_event(event)
+    begin
+      Landlord::Account.update_from_stripe_subscription_event(event)
+    rescue => ex
+      SupportMailer.notification('Stripe event error', "#{ex.message}").deliver_later
+    end
   end
 
   # Occurs whenever a new charge is created and is successful.
   events.subscribe 'charge.succeeded' do |event|
     charge = event.data.object
-    receipt = Landlord::Receipt.save_from_stripe(charge)
-    if receipt
-      Landlord::ReceiptMailer.succeeded(receipt).deliver_later
-    end
+    receipt = Landlord::Receipt.save_from_stripe(charge) if charge
+    Landlord::ReceiptMailer.succeeded(receipt).deliver_later if receipt
   end
 
   # Occurs whenever a failed charge attempt occurs.
   events.subscribe 'charge.failed' do |event|
     charge = event.data.object
-    receipt = Landlord::Receipt.save_from_stripe(charge)
-    if receipt
-      Landlord::ReceiptMailer.failed(receipt).deliver_later
-    end
+    receipt = Landlord::Receipt.save_from_stripe(charge) if charge
+    Landlord::ReceiptMailer.failed(receipt).deliver_later if receipt
   end
 
   # Occurs whenever a charge is refunded, including partial refunds.
   events.subscribe 'charge.refunded' do |event|
     charge = event.data.object
-    receipt = Landlord::Receipt.save_from_stripe(charge)
-    if receipt
-      Landlord::ReceiptMailer.refunded(receipt).deliver_later
-    end
+    receipt = Landlord::Receipt.save_from_stripe(charge) if charge
+    Landlord::ReceiptMailer.refunded(receipt).deliver_later if receipt
   end
 
   # Occurs whenever a customer disputes a charge with their bank (chargeback).
